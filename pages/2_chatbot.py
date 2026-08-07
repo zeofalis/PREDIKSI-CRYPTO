40c79f621087af38ae2ec06f5ddcbc85b76069f4
zeofalis <zeofalis17@gmail.com>
2026-08-08 04:36:22 +0700

    Update 2_chatbot.py

--- pages/2_chatbot.py
+++ pages/2_chatbot.py
@@ -121,14 +121,10 @@ section[data-testid="stSidebar"] {
 # GROQ CLIENT
 # =====================================
 try:
-    api_key = st.secrets["GROQ_API_KEY"]
-
-    st.success(f"API Key terbaca: {api_key[:10]}...")
-
-    client = Groq(api_key=api_key)
-
-except Exception as e:
-    st.error(f"Secrets Error: {e}")
+    client = Groq(
+        api_key=st.secrets["GROQ_API_KEY"]
+    )
+except Exception:
     client = None
 
 # =====================================
